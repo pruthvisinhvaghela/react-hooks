0ef7b01f37f2e4bb008287d2dcacb92567fad0f6
pruthvisinhvaghela <pruthvivaghela19@gmail.com>
2024-02-07 15:26:15 +0530

    todo created

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,38 @@ import "./App.css";
 function App() {
   const [todo, setTodo] = useState([]); //hold the list of todo
   const [input, setInput] = useState(""); //hold the value of the input field
+
+  const change = (e) => {
+    setInput(e.target.value);
+  };
+
+  const submit = (e) => {
+    e.preventDefault();
+    setTodo([...todo, input]);
+    setInput("");
+  };
   return (
     <>
       <div className="container">
         <h1>Todo List</h1>
         <div className="main-box">
           <form action="">
-            <input type="text" placeholder=" enter your task" />
-            <button>Add todo</button>
+            <input
+              type="text"
+              placeholder=" enter your task"
+              value={input}
+              onChange={change}
+            />
+            <button onClick={submit}>Add todo</button>
           </form>
+
+          <ul>
+            {todo.map((td) => (
+              <li key={td} className="input">
+                {td}
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </>
