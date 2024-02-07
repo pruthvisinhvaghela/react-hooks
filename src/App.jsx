7b714afabb56cbe51226b58af5347a2cf8170c10
pruthvisinhvaghela <pruthvivaghela19@gmail.com>
2024-02-07 19:09:54 +0530

    comment added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,48 @@
-import { useState, useMemo } from "react"; //use for increase performance
+import { useReducer } from "react";
 import "./App.css";
 
+const initialState = 15;
+const reducer = (state, action) => {
+  switch (action) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+/*
+step 1 we import useReducer
+then we pass parameter reducer and initialState;
+then we set  initialState as 0 
+then in reducer func we pas two parameter current state and action 
+then useReducer return count and dispatch ;
+we use count for print num 
+then we use dispatch so it will go to action and then check the value
+
+*/
+
 function App() {
-  return <></>;
+  const [count, dispatch] = useReducer(reducer, initialState);
+  return (
+    <>
+      <div>count = {count}</div>
+      <button
+        onClick={() => {
+          dispatch("increment");
+        }}>
+        increment
+      </button>
+      <button
+        onClick={() => {
+          dispatch("decrement");
+        }}>
+        decrement
+      </button>
+    </>
+  );
 }
 
 export default App;
