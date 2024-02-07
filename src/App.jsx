031af66cd220e0c5ce6694568c7ed2c7ae307130
pruthvisinhvaghela <pruthvivaghela19@gmail.com>
2024-02-07 19:11:16 +0530

    comment edit

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ step 1 we import useReducer
 then we pass parameter reducer and initialState;
 then we set  initialState as 0 
 then in reducer func we pas two parameter current state and action 
-then useReducer return count and dispatch ;
+then useReducer return count and dispatch // call reducer fun and return new state 
 we use count for print num 
 then we use dispatch so it will go to action and then check the value
 
