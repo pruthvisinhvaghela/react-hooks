112e254cb4fcc3d70d7b25a87e231d51ac24d906
pruthvisinhvaghela <pruthvivaghela19@gmail.com>
2024-02-07 14:50:32 +0530

    input field created

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,16 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [todo, setTodo] = useState();
-
-  return <></>;
+  const [todo, setTodo] = useState([]); //hold the list of todo
+  const [input, setInput] = useState(""); //hold the value of the input field
+  return (
+    <>
+      <div>
+        <h1>Todo List</h1>
+        <input type="text" placeholder=" enter your task" />
+      </div>
+    </>
+  );
 }
 
 export default App;
