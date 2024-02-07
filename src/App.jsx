9f09d7d6669efc22bf896be4d311b312de820db3
pruthvisinhvaghela <pruthvivaghela19@gmail.com>
2024-02-07 14:47:02 +0530

    todo added

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,11 @@
 import { useState } from "react";
+
 import "./App.css";
 
 function App() {
-  const [color, setColor] = useState("green");
-  //green we can change
-  return (
-    <>
-      <div className="main-screen" style={{ backgroundColor: color }}>
-        <div className="menubar">
-          <div className="btn-menu">
-            <button onClick={() => setColor("red")} id="red">
-              Red
-            </button>
-            <button onClick={() => setColor("blue")} id="blue">
-              Blue
-            </button>
-            <button onClick={() => setColor("Orange")} id="orange">
-              Orange
-            </button>
-            <button
-              onClick={() => setColor("rebeccapurple")}
-              id="rebeccapurple">
-              Purple
-            </button>
+  const [todo, setTodo] = useState();
 
-            <button onClick={() => setColor("pink")} id="pink">
-              Pink
-            </button>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
