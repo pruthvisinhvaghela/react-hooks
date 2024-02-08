9bc3622854383ffe40540eae9255dc2012ca20b2
pruthvisinhvaghela <pruthvivaghela19@gmail.com>
2024-02-08 11:24:07 +0530

    use ref example

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,26 @@
-import { useReducer } from "react";
-import "./App.css";
+import { useRef } from "react";
 
-const initialState = 0;
-const reducer = (state, action) => {
-  switch (action) {
-    case "increment":
-      return state + 1;
-    case "decrement":
-      return state - 1;
-    default:
-      return state;
-  }
-};
+const App = () => {
+  // Initializing a ref
+  const inputRef = useRef(null);
 
-/*
-step 1 we import useReducer
-then we pass parameter reducer and initialState;
-then we set  initialState as 0 
-then in reducer func we pas two parameter current state and action 
-then useReducer return count and dispatch // call reducer fun and return new state 
-we use count for print num 
-then we use dispatch so it will go to action and then check the value
+  // Function to focus on the input element
+  const focusInput = () => {
+    // Accessing the current property of the ref to get the DOM node
+    inputRef.current.focus();
+  };
 
-*/
-
-function App() {
-  const [count, dispatch] = useReducer(reducer, initialState);
   return (
-    <>
-      <div>count = {count}</div>
-      <button
-        onClick={() => {
-          dispatch("increment");
-        }}>
-        increment
-      </button>
-      <button
-        onClick={() => {
-          dispatch("decrement");
-        }}>
-        decrement
-      </button>
-    </>
+    <div>
+      <h1>Hello, React!</h1>
+      <input
+        type="text"
+        ref={inputRef} // Assigning the ref to the input element
+        placeholder="Type something..."
+      />
+      <button onClick={focusInput}>Focus Input</button>
+    </div>
   );
-}
+};
 
 export default App;
